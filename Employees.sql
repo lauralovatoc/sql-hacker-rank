ae241b58d612dd0cfe7a1d2aff2bb691e421b060
Laura Lovato Camponogara <103641723+lauralovatoc@users.noreply.github.com>
2025-02-22 11:06:05 -0300

    Create Employees.sql

---
+++ Employees.sql
@@ -0,0 +1,5 @@
+/* Employee Names */
+SELECT name FROM employee ORDER BY name ASC;
+
+/* Employee Salaries */
+SELECT name FROM employee WHERE salary > 2000 AND months < 10 ORDER BY employee_id ASC;
